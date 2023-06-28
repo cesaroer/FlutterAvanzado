951555be6371ac1918f29ae4ed1ac871f3df9f38
cesaroer <cesarvarcard@gmail.com>
2023-06-27 20:38:28 -0600

    Added socket io

--- 01-band_names_server/index.js
+++ 01-band_names_server/index.js
@@ -1,15 +1,37 @@
-const express = require("express");
+//importa el .env
+require('dotenv').config();
+
+//const { errorMonitor } = require("events");
+//Se instancia el express
+const express = require('express');
 const app = express();
-require("dotenv").config();
-const path = require("path");
+
+//se importa la lib del path
+const path = require('path');
+
+//se crea el servidor node
+const httpServer = require("http").createServer(app);
 
 
-// Path Publico
+//se importa el socket.io
+const io = require("socket.io")(httpServer);
+
+
+//mensajes io de socket.io
+io.on("connection", client => {
+    console.log('Cliente conectado :D');
+    client.on('disconnect', () => {
+        console.log('Cliente desconectado :(');
+    });
+});
+
+//path publico
 const publicPath = path.resolve(__dirname, 'public');
 app.use(express.static(publicPath));
 
 
-app.listen(process.env.PORT, (err) => {
+//listen server in port
+httpServer.listen(process.env.PORT, (err) => {
     if (err) throw new Error(err);
-    console.log('Servidor corriendo en puerto!!!', process.env.PORT);
+    console.log('Servidor corriendo en puerto!!', process.env.PORT);
 });
