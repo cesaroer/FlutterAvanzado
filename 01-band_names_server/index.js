85b30ac9445ebdeaaf1e89043f1f7f82f363f2c0
cesaroer <cesarvarcard@gmail.com>
2023-06-27 20:18:14 -0600

    Added env and dotenv

--- 01-band_names_server/index.js
+++ 01-band_names_server/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+require("dotenv").config();
 const path = require("path");
 
 
@@ -8,7 +9,7 @@ const publicPath = path.resolve(__dirname, 'public');
 app.use(express.static(publicPath));
 
 
-app.listen(3000, (err) => {
+app.listen(process.env.PORT, (err) => {
     if (err) throw new Error(err);
-    console.log('Servidor corriendo en puerto!!!', 3000);
+    console.log('Servidor corriendo en puerto!!!', process.env.PORT);
 });
