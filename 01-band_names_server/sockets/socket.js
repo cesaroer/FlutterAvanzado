9067a0168cebc90cb92083bb3cffe8ae95c79f79
cesaroer <cesarvarcard@gmail.com>
2023-06-29 18:18:59 -0600

    Added new methods to seee in console of browser

--- 01-band_names_server/sockets/socket.js
+++ 01-band_names_server/sockets/socket.js
@@ -1,8 +1,22 @@
 const { io } = require("../index");
+const Bands = require('../models/bands');
+const Band = require('../models/band');
+
+const bands = new Bands();
+
+bands.addBand(new Band("Queen"));
+bands.addBand(new Band("Bon Jovi"));
+bands.addBand(new Band("Enanitos Verdes"));
+bands.addBand(new Band("Metallika"));
+
 
 //mensajes io de socket.io
 io.on("connection", client => {
     console.log('Cliente conectado :D');
+
+    client.emit("active-bands", bands.getBands());
+
+
     client.on('disconnect', () => {
         console.log('Cliente desconectado :(');
     });
