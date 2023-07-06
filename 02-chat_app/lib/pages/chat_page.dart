1634ba039ff358d7d651c2a0cd6fa7de04d9a814
cesaroer <cesarvarcard@gmail.com>
2023-07-05 23:03:09 -0600

    Forst part of chat design

--- 02-chat_app/lib/pages/chat_page.dart
+++ 02-chat_app/lib/pages/chat_page.dart
@@ -4,8 +4,47 @@ class ChatPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text("ChatPage"),
+      appBar: AppBar(
+        elevation: 1,
+        backgroundColor: Colors.white,
+        title: Column(
+          children: [
+            CircleAvatar(
+              maxRadius: 14,
+              backgroundColor: Colors.blueAccent,
+              child: Text(
+                "TE",
+                style: TextStyle(fontSize: 12),
+              ),
+            ),
+            SizedBox(height: 3),
+            Text(
+              "Test User",
+              style: TextStyle(fontSize: 12, color: Colors.black87),
+            ),
+          ],
+        ),
+        centerTitle: true,
+      ),
+      body: Container(
+        child: Column(
+          children: [
+            Flexible(
+              child: ListView.builder(
+                physics: BouncingScrollPhysics(),
+                itemBuilder: (BuildContext context, int index) {
+                  return Text("$index");
+                },
+                reverse: true,
+              ),
+            ),
+            Divider(height: 2),
+            Container(
+              color: Colors.white,
+              height: 100,
+            ),
+          ],
+        ),
       ),
     );
   }
