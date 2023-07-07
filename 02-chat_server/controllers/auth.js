f0dd728fd994ed308c2b64e90105e61ed22ce17a
cesaroer <cesarvarcard@gmail.com>
2023-07-06 21:38:29 -0600

    Added validations in middle wares

--- 02-chat_server/controllers/auth.js
+++ 02-chat_server/controllers/auth.js
@@ -3,14 +3,7 @@ const { validationResult } = require("express-validator");
 
 const crearUsuario = (req, resp = response) => {
 
-    const errors = validationResult(req);
 
-    if (!errors.isEmpty()) {
-        return resp.status(400).json({
-            ok: false,
-            errors: errors.mapped()
-        });
-    }
 
     resp.json({
         ok: true,
--- 02-chat_server/routes/auth.js
+++ 02-chat_server/routes/auth.js
@@ -3,10 +3,15 @@
 const { Router } = require("express");
 const { check } = require("express-validator");
 const { crearUsuario } = require("../controllers/auth");
+const { validarCampos } = require("../middlewares/validate-fields");
 const router = Router();
 
 router.post("/new", [
-    check("nombre", "El nombre es obligatorio").not().isEmpty()
+    check("nombre", "El nombre es obligatorio").not().isEmpty(),
+    check("email", "El email no es valido").isEmail(),
+    check("password", "El password no es valido").not().isEmpty(),
+
+    validarCampos
 ], crearUsuario);
 
 module.exports = router;
