0b1a687ecdc82bf50be6f03e7b1013451a46ebda
cesaroer <cesarvarcard@gmail.com>
2023-07-06 21:29:00 -0600

    Added middle ware to error handle

--- 02-chat_server/routes/auth.js
+++ 02-chat_server/routes/auth.js
@@ -1,10 +1,13 @@
 // path: api/login/new
 
 const { Router } = require("express");
+const { check } = require("express-validator");
 const { crearUsuario } = require("../controllers/auth");
 const router = Router();
 
-router.post("/new", crearUsuario);
+router.post("/new", [
+    check("nombre", "El nombre es obligatorio").not().isEmpty()
+], crearUsuario);
 
 module.exports = router;
 
